365f63a70b66fa8e04d388e4d1211eb0a0846749
andersbandt <andersbandt@yahoo.com>
2021-03-22 00:05:42 -0500

    Create sensorControl.py

---
+++ envControl/sensorControl.py
@@ -0,0 +1,52 @@
+import serialManager
+import time
+
+
+def readPH():
+        serialManager.sendChar('P')
+        time.sleep(.5)
+        pH = serialManager.readLine()
+        print("pH: ")
+        print(pH)
+        return pH
+
+def readWaterTemp():
+        serialManager.sendChar('W')
+        time.sleep(.5)
+        waterTemp = serialManager.readLine()
+        print("Water temperature: ")
+        print(waterTemp)
+        return waterTemp
+
+def readTemp():
+        serialManager.sendChar('T')
+        time.sleep(.5)
+        temp = serialManager.readLine()
+        print("Air temp: ")
+        print(temp)
+        return temp
+
+def readEC():
+        serialManager.sendChar('E')
+        time.sleep(.5)
+        EC = serialManager.readLine()
+        print("EC: ")
+        print(EC)
+        print("ms/cm")
+        return EC
+
+def readTDS():
+        serialManager.sendChar('D')
+        time.sleep(.5)
+        TDS = serialManager.readLine()
+        print("TDS: ")
+        print(TDS)
+        print("ppm")
+        return TDS
+
+
+readPH()
+readWaterTemp()
+readTemp()
+readEC()
+readTDS()
