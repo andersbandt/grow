ca15376c0fcaa2349f32b44a210682e868028b79
andersbandt <andersbandt@yahoo.com>
2021-03-22 00:03:00 -0500

    Create tankManager.py

---
+++ envControl/tankManager.py
@@ -0,0 +1,14 @@
+import pumpControl.py
+import airControl.py
+import sensorControl.py
+import pHControl.py
+
+
+
+
+def fillTank():
+  print("Starting tank filling process")
+  
+  
+def emptyTank():
+   print("Starting tank emptying process")
