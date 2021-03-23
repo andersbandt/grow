66e94da5e3d965d1fd82038fdeea84879c7b4f08
andersbandt <andersbandt@yahoo.com>
2021-03-23 01:03:44 -0500

    Create main.py

---
+++ flask/main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Index"
+  
+@app.route("/control")
+def control():
+    return "Control"
+  
+
+  
+  
+if __name__ == "__main__":
+  app.run()
