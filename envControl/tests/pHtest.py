ce8503998d3857ed10da9cc93315b58e83e1f9c3
andersbandt <andersbandt@yahoo.com>
2021-04-09 21:26:18 -0500

    On branch master  Changes to be committed: 	new file:   pHtest_data.csv 	deleted:    pHtest_data.txt 	modified:   tests/__pycache__/pHtest.cpython-37.pyc 	modified:   tests/pHtest.py

--- envControl/tests/pHtest.py
+++ envControl/tests/pHtest.py
@@ -2,11 +2,12 @@ from control import pHControl
 from control import pumpControl
 from control import sensorControl
 import time
+import csv
 
-dosing_interval = 5 # amount of time between acid/base additions in minutes
+dosing_interval = 1 # amount of time between acid/base additions in minutes
 pumpSeconds = 20 # amount of seconds to run the pump each dosing interval
 pH_interval = 15 # interval between pH readings in seconds
-test_time = 30*60 # total testing time in seconds
+test_time = 5*60 # total testing time in seconds
 pH_readings = int(dosing_interval*60/pH_interval) # number of pH readings between acid/base addition
 
 def main():
@@ -14,18 +15,19 @@ def main():
 	cur_time = 0
 	print("BTW... pH_readings is ", pH_readings)
 	doc = open('pHtest_data.txt', 'w')
+	writer = csv.writer(doc)
 	while cur_time < (start_time + test_time):
 			pumpControl.pumpFluid('acid', pumpSeconds)
 			cur_time = time.time()
-			doc.write(str(cur_time - start_time) + '\n')
-			doc.write("Acid: " + str(pumpSeconds) + '\n')
+			writer.writerow(str(cur_time - start_time) + '\n')
+			writer.writerow("Acid: " + str(pumpSeconds) + '\n')
 
 			i = 0
 			for i in range(pH_readings):
 				pH = sensorControl.readpH()
 				cur_time = time.time()
-				doc.write(str(cur_time - start_time) + '\n')
-				doc.write(str(pH) + '\n')
+				writer.writerow(str(cur_time - start_time) + '\n')
+				writer.writerow(str(pH) + '\n')
 				time.sleep(pH_interval)
 				i = i + 1
 
