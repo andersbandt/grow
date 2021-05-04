c21f773337d2282f7205e6d15b0d39e78f43341a
andersbandt <andersbandt@yahoo.com>
2021-05-04 10:57:04 -0500

    On branch master

--- scheduler/updateSchedule.py
+++ scheduler/updateSchedule.py
@@ -13,16 +13,18 @@ def updateSchedule(flag, *args):
 		True
 
 	elif flag == -1: # if no inputs are being passed, prompt user
-		area = int(input("Would you like to update: 1-Lights, 2-Outtake, 3-Intake, 4-Air pump, 5-Water pump"))
-		if area ==1:
-			area = "Light"
-	if area == 1: # set light schedule
+		area_num = int(input("Would you like to update: 1-Lights, 2-Outtake, 3-Intake, 4-Air pump, 5-Water pump"))
+
+	if area_num == 1: # set light schedule
+		area = "Light"
 		num = int(input("Two options: 1-Time that light turns on, 2-Duration light is on"))
 		value = int(input("Please enter the desired parameter value"))
-	elif area == 2: # update outtake schedule
+	elif area_num == 2: # update outtake schedule
+		area = "Outtake"
 		num = int(input("Two options: 1-Duration fan is on, 2-Duration fan is off"))
 		value = int(input("Please enter the desired parameter value"))
-	elif area == 3: # update intake schedule
+	elif area_num == 3: # update intake schedule
+		area = "Intake"
 		num = int(input("Two options: 1-Duration fan is on, 2-Duration fan is off"))
 		value = int(input("Please enter the desired parameter value"))
 
@@ -37,7 +39,6 @@ def updateSchedule(flag, *args):
 
 def main():
   return True
-  
 
 if __name__ == "__main__":
   main()
